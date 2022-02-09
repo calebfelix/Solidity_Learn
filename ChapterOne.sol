7935f3043b63f440bc7164d5271242d306d9e8f5
Caleb Felix <calebfelix38@gmail.com>
2022-02-09 18:00:40 +0530

    Update ChapterOne.sol

--- ChapterOne.sol
+++ ChapterOne.sol
@@ -4,8 +4,8 @@ contract ChapterOne {
     // 1. fixed size type
     bool isReady;
     uint a;
-    address myAdd;
-    bytes32 data;
+    address myAdd; 
+    bytes32 data; // usually used for any binary data commonly strings as there are not many string operations in solidity
 
     // 2. variable size type
     string name;
